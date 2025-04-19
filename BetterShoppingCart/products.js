09f2aeaf3a06207345de2148e66159aa88c51441
Wina07 <elwinasuarez1207@gmail.com>
2025-04-19 11:19:27 +0800

    Add files via upload

---
+++ BetterShoppingCart/products.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+        id: 1,
+        title: "HILEE (Pear) Premium Flask Hydration",
+        price: "PHP 341.91",
+        description: "Warming Time: 7-12 Hours Cooling Time: 12-24 Hours  Style: Classic Material: Stainless Steel Feature: Leak-proof, Heat Resistant, Anti-Scalding Volume: 20oz-43oz Quantity Per Pack: 1",
+        colors: [
+            {
+                name:"Pear",
+                mainImage: "pear.jpg",
+                thumbnails: [
+                    "pearstand.png",
+                    "hotncold-pear.png",
+                    "fourfruit.jpg"
+                ],
+                sizes: ["20oz", "27oz", "33oz", "43oz"],
+            }   
+        ]
+    },
+{
+    id: 2,
+    title: "HILEE (Wax Apple) Premium Flask Hydration",
+    price: "PHP 341.91",
+    description: "Warming Time: 7-12 Hours Cooling Time: 12-24 Hours  Style: Classic Material: Stainless Steel Feature: Leak-proof, Heat Resistant, Anti-Scalding Volume: 20oz-43oz Quantity Per Pack: 1",
+        colors: [
+            {
+                name:"Wax Apple",
+                mainImage: "wax apple.jpg",
+                thumbnails: [
+                    "waxapplestand.png",
+                    "hotncold-waxapple.png",
+                    "fourfruit.jpg"
+                ],
+                sizes: ["20oz", "27oz", "33oz", "43oz"],
+            }   
+        ]
+    },
+    {
+        id: 3,
+        title: "HILEE (Pineapple) Premium Flask Hydration",
+        price: "PHP 341.91",
+        description: "Warming Time: 7-12 Hours Cooling Time: 12-24 Hours  Style: Classic Material: Stainless Steel Feature: Leak-proof, Heat Resistant, Anti-Scalding Volume: 20oz-43oz Quantity Per Pack: 1",
+            colors: [
+                {
+                        name:"Pineapple",
+                mainImage: "pineapple.jpg",
+                thumbnails: [
+                    "pineapplestand.png",
+                    "hotncold-pineapple.jpg",
+                    "fourfruit.jpg"
+                    ],
+                    sizes: ["20oz", "27oz", "33oz", "43oz"],
+                }   
+        ]
+    },
+    {
+        id: 4,
+        title: "HILEE (Green Apple) Premium Flask Hydration",
+        price: "PHP 341.91",
+        description: "Warming Time: 7-12 Hours Cooling Time: 12-24 Hours  Style: Classic Material: Stainless Steel Feature: Leak-proof, Heat Resistant, Anti-Scalding Volume: 20oz-43oz Quantity Per Pack: 1",
+            colors: [
+                {
+                    name:"Green Apple",
+                    mainImage: "green apple.jpg",
+                    thumbnails: [
+                        "greenapplestand.jpg",
+                        "hotncold-green apple.png",
+                        "fourfruit.jpg"
+                    ],
+                    sizes: ["20oz", "27oz", "33oz", "43oz"],
+                }   
+        ]
+    },  
+];
