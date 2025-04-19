886380f87ffb31a941e504936b7615316bbfb214
mjvelasco03 <mjazper03@gmail.com>
2025-04-19 10:11:46 +0800

    Add files via upload

---
+++ PAYMENT/Payment Method.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        body{
+            background-image: linear-gradient(#DFC5FE, #ADD8E6, #DFC5FE, #ADD8E6);;;
+        }
+    </style>
+</head>
+<body>
+    <div class="lagayan">
+    <div class="frontlogo">
+        <a href="#"> <input type="button" name="BACK" class="bbbbt" value="BACK"></a>
+    <img src="lologo.png">
+    </div>
+    <div class="mj">
+        <h1>Payment Method</h1>
+    </div>
+    <br>
+    <div class="velas">
+        <h3>Select Payment</h3>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="choose">
+    <a href="Gcash Payment.php"><img src="ol.png"></a>
+    <div class="la"><p>OR</p></div>
+    <a href="CashD.php"></hilee><img src="cd.jpg"></a>
+</div>
+</div>
+</body>
+</html>
