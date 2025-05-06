36ee86b5ee7d44e7fc17d5daa7f34f549990020e
MasterMoiRi <moiricka09@gmail.com>
2025-05-06 18:26:26 +0800

    Update delete.php

--- ERICKA/crud remove/delete.php
+++ ERICKA/crud remove/delete.php
@@ -5,8 +5,8 @@ $conn = new mysqli("localhost", "root", "", "simple_crud");
 $id = $_GET["id"];
 
 if ($conn->query("DELETE FROM users WHERE id=$id")) {
-    header("Location: index.php?message=deleted");
+    header("Location: read.php?message=deleted");
 } else {
-    header("Location: index.php?message=error");
+    header("Location: read.php?message=error");
 }
 ?>
